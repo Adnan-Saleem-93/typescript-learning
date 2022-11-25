3fd71e6efb07a61de5cff823b21236d2d7dd5aed
Adnan Saleem <adnan13893@gmail.com>
2022-11-26 00:46:54 +0500

    Number Types

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var randomArr = ['World.!!!', 'Hello', 10];
-// Typescript inferred 'randomArr' type as (string | number)[]
+var message = 'Number Type';
+// there are 2 types of numbers in TS/JS
+// 1. number/floating-point
+// 2. bigint/big integer
+// number types
+var decimal = 200;
+var binary = 1;
+var octal = 10;
+var hexadecimal = 0xb;
+// decimal =  9007199254740991n
+// in above example, value of type 'bigint' is being assigned to 'decimal' whose type was inferred as 'number' by TS, so this will cause an error
+// big int
+// let bigNumber = 9007199254740991n
+// bigNumber = 0.6
+// similarly, in above example, value of type 'number' is being assigned to 'bigNumber' whose type was inferred as 'bigint' by TS, so this will cause an error
 var header = document.createElement('h1');
-header.textContent = "".concat(randomArr[1], " ").concat(randomArr[0]);
-// randomArr = ['h1', 1, false]
-// the above will result in an error because the third element is a boolean value, which is not allowed in type (string | number)[]
+header.textContent = message;
 document.body.appendChild(header);
--- index.ts
+++ index.ts
@@ -1,11 +1,25 @@
-let randomArr = ['World.!!!', 'Hello', 10]
-// Typescript inferred 'randomArr' type as (string | number)[]
+let message = 'Number Type'
+
+// there are 2 types of numbers in TS/JS
+// 1. number/floating-point
+// 2. bigint/big integer
+
+// number types
+let decimal = 200
+let binary = 0b01
+let octal = 0o12
+let hexadecimal = 0xb
+
+// decimal =  9007199254740991n
+// in above example, value of type 'bigint' is being assigned to 'decimal' whose type was inferred as 'number' by TS, so this will cause an error
+
+// big int
+// let bigNumber = 9007199254740991n
+// bigNumber = 0.6
+// similarly, in above example, value of type 'number' is being assigned to 'bigNumber' whose type was inferred as 'bigint' by TS, so this will cause an error
 
 let header = document.createElement('h1')
 
-header.textContent = `${randomArr[1]} ${randomArr[0]}`
-
-// randomArr = ['h1', 1, false]
-// the above will result in an error because the third element is a boolean value, which is not allowed in type (string | number)[]
+header.textContent = message
 
 document.body.appendChild(header)
