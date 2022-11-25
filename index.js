1595d44fd3a1aa9249f19f55efb0869cfcf04899
Adnan Saleem <adnan13893@gmail.com>
2022-11-26 00:20:26 +0500

    Type Inference

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var printHelloWithName = function (name) {
-    return "Hello ".concat(name);
+var printHelloWithName = function (text) {
+    return "Hello ".concat(text);
 };
+var message = 'World';
 var header = document.createElement('h1');
-header.textContent = printHelloWithName('People.!');
+header.textContent = printHelloWithName(message);
+// message = 0 -> this iwll cause an error because when we assigned 'World' to 'message' variable, TS inferred its type to string. So 'message' can only be assigned string values.
 document.body.appendChild(header);
--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
-let printHelloWithName: (name: string) => string = (name: string) => {
-  return `Hello ${name}`
+let printHelloWithName: (text: string) => string = (text: string) => {
+  return `Hello ${text}`
 }
+
+let message = 'World'
 
 let header = document.createElement('h1')
 
-header.textContent = printHelloWithName('People.!')
+header.textContent = printHelloWithName(message)
+
+// message = 0 -> this iwll cause an error because when we assigned 'World' to 'message' variable, TS inferred its type to string. So 'message' can only be assigned string values.
 
 document.body.appendChild(header)
