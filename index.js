fa0f5a5947d332fbb76893bc9f32abcfc57b0636
Adnan Saleem <adnan13893@gmail.com>
2023-05-23 12:49:04 +0500

    function parameters and return type

---
+++ index.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = ["User"];
+var output = greeter(user);
+console.log(output);
---
+++ index.ts
@@ -0,0 +1,8 @@
+function greeter(person: string): string {
+	return "Hello, " + person;
+}
+
+let user: string = "User";
+
+const output = greeter(user);
+console.log(output);
