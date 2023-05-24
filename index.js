d7e40dfd1d1996f2172f9287febdf8c54734063b
Adnan Saleem <adnan13893@gmail.com>
2023-05-24 20:31:18 +0500

    readonly and optional properties in interface

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-var Person = /** @class */ (function () {
-    function Person(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.fullName = "".concat(firstName, " ").concat(lastName);
-    }
-    return Person;
-}());
-var person = new Person("John", "Doe");
-var greetUser = function (user) {
-    return "Hello ".concat(user.firstName, " ").concat(user.lastName);
+var user = {
+    fullName: { firstName: "Jon", lastName: "Snow" },
+    age: 22
 };
-var consoleUser = function (user) {
-    console.log("Hello ".concat(user.firstName, " ").concat(user.lastName));
-};
-var newPerson = new Person("Jon", "Snow");
-consoleUser(newPerson);
+console.log(user);
--- index.ts
+++ index.ts
@@ -1,24 +1,10 @@
-class Person {
-	fullName: string;
-	constructor(public firstName: string, public lastName: string) {
-		this.fullName = `${firstName} ${lastName}`;
-	}
-}
-
-// Interfaces
 interface User {
-	firstName: string;
-	lastName: string;
+	readonly fullName: { readonly firstName: string; lastName: string };
+	age?: number;
 }
-
-const person = new Person("John", "Doe");
-
-const greetUser = (user: User): string => {
-	return `Hello ${user.firstName} ${user.lastName}`;
-};
-const consoleUser = (user: User): void => {
-	console.log(`Hello ${user.firstName} ${user.lastName}`);
+const user: User = {
+	fullName: { firstName: "Jon", lastName: "Snow" },
+	age: 22,
 };
 
-const newPerson = new Person("Jon", "Snow");
-consoleUser(newPerson);
+console.log(user);
